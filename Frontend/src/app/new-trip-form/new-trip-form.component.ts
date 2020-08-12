1b5ddb461e1bd14a13ae4445bf9b4dec3a8218b2
Yaroslav Ishanov <ishanovyaro@gmail.com>
2020-08-12 16:25:02 +0300

    moved navbar to app.component and fixed link to new trip form

--- Frontend/src/app/new-trip-form/new-trip-form.component.ts
+++ Frontend/src/app/new-trip-form/new-trip-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TripsService, TripDTO } from 'matkalasku-api-client';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-new-trip-form',
@@ -6,6 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-trip-form.component.css']
 })
 export class NewTripFormComponent implements OnInit {
+  trip: TripDTO = {
+    companyId: 0,
+    departure: new Date(),
+    recurrence: new Date(),
+    recipient: '',
+    purpose: '',
+    distanceInKM: 0,
+    locationDeparture: '',
+    locationDestination: '',
+    description: '',
+    passengerCount: 0
+  };
+  departureDate: NgbDateStruct;
+  recurrenceDate: NgbDateStruct;
 
   constructor() { }
 
